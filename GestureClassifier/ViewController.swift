aed3e156090199c675a4c89ae41a739c6d79d1ae
Abishkar Chhetri <abishkarchhetri@dhcp-18-40-26-57.dyn.mit.edu>
2019-04-14 23:39:48 -0400

    Read CSV files and create an internal representation of the data

--- GestureClassifier/ViewController.swift
+++ GestureClassifier/ViewController.swift
@@ -25,17 +25,105 @@ class ViewController: UIViewController {
             return nil
         }
     }
+    
+    func listFilesInDirectory(path:String) -> Array<String> {
+        var files = Array<String>()
+        
+        let fm = FileManager.default
+        let root_path = Bundle.main.resourcePath!
+        do {
+            let items = try fm.contentsOfDirectory(atPath: root_path + "/" + path)
+            
+            for item in items {
+                files.append(item)
+            }
+        } catch {
+            // failed to read directory – bad permissions, perhaps?
+            print("Unexpected error: \(error).")
+        }
+        return files
+    }
+    
+    func populateValues(sampleData:String, sample:Sample) -> () {
+        var dataArray = sampleData.components(separatedBy: "\n")
+        dataArray.removeFirst(1)
+        for row in dataArray {
+            if row.count > 0 {
+                let rowArray = row.components(separatedBy: ",").map {Int($0)}
+                sample.accX.append(rowArray[0]!)
+                sample.accY.append(rowArray[1]!)
+                sample.accZ.append(rowArray[2]!)
+                sample.gyrX.append(rowArray[3]!)
+                sample.gyrY.append(rowArray[4]!)
+                sample.gyrZ.append(rowArray[5]!)
+            }
+
+        }
+    }
+    
+    func createDataDict(path:String) -> Dictionary<String, Participant> {
+        var data: [String: Participant] = [:]
+        let participantFiles = listFilesInDirectory(path: path)
+        
+        for participantfileName in participantFiles { //P1-FT, P2-LT, ....
+            let participantfileNameArr = participantfileName.components(separatedBy: "-")
+            let participantName = participantfileNameArr[0]
+            var gestureDirection = participantfileNameArr[1]
+            
+            let participantPath = path+"/"+participantfileName
+            let sampleFiles = listFilesInDirectory(path: participantPath)
+            
+            if !data.keys.contains(participantName) {
+                data[participantName] = Participant(name: participantName)
+            }
+            
+            let participant = data[participantName]!
+            
+            var participantGesture = Array<Sample>()
+            
+            for sampleFileName in sampleFiles {
+                let sampleFileNameWithoutExt = sampleFileName.components(separatedBy: ".")[0]
+                let sampleData = readDataFromCSV(fileName: sampleFileNameWithoutExt, fileType: "csv", path: participantPath)
+                
+                let sampleFileNameWithoutExtArr = sampleFileNameWithoutExt.components(separatedBy: "-")
+                
+                var sample:Sample
+    
+                if sampleFileNameWithoutExtArr.count == 2 {
+                    sample = Sample(number: Int(sampleFileNameWithoutExtArr[1])!)
+                } else {
+                    sample = Sample(number: 0)
+                }
+                populateValues(sampleData: sampleData!, sample: sample)
+                participantGesture.append(sample)
+            }
+            
+            switch gestureDirection {
+            case "FT":
+                participant.frontSamples = participantGesture
+            case "LT":
+                participant.leftSamples = participantGesture
+            case "RT":
+                participant.rightSamples = participantGesture
+            default:
+                print("Wrong Value in Participant Gesture")
+                participantGesture = Array<Sample>()
+            }
+        }
+        
+        
+        return data
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //testKNN()
-        
-        let path = "data_csv/P1-FT/"
-        let filename = "front-1"
-        
-        var data = readDataFromCSV(fileName: filename, fileType: "csv", path: path)
-        print(data)
+        let data = createDataDict(path: "data_csv")
+//        print("keys: ",data.keys)
+//        print(data["P5"])
+//        print(data["P5"]?.leftSamples)
+//        print(data["P5"]?.leftSamples.count)
+//        print(data["P5"]?.leftSamples[5].accX)
     }
 
     
@@ -83,6 +171,33 @@ class ViewController: UIViewController {
         print("predicted " + prediction.label, "with ", prediction.probability*100,"% certainty")
         print("predicted " + prediction2.label, "with ", prediction2.probability*100,"% certainty")
     }
-
 }
 
+class Participant {
+    
+    var name:String
+    var leftSamples = Array<Sample>()
+    var rightSamples = Array<Sample>()
+    var frontSamples = Array<Sample>()
+    
+    init(name:String) {
+        self.name=name
+    }
+    
+    
+}
+
+class Sample {
+    
+    var number:Int = 0
+    init(number:Int) {
+        self.number = number
+    }
+    
+    var accX = Array<Int>()
+    var accY = Array<Int>()
+    var accZ = Array<Int>()
+    var gyrX = Array<Int>()
+    var gyrY = Array<Int>()
+    var gyrZ = Array<Int>()
+}
